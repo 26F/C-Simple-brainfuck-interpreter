45d72d50a61436138aeab512dc59eff36a2c37f2
26F <32969567+26F@users.noreply.github.com>
2021-06-30 20:05:58 +1200

    Update brainfuck_monster.c

--- brainfuck_monster.c
+++ brainfuck_monster.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,7 +5,9 @@ int main(int argc, char ** argv) {
 	unsigned char tape[30000] = {0}; // tape of 30 unsigned chars
 	int cell = 0;                    // target given cell
 	int insp = 0;                    // where in fstring is given char ("instruction pointer")
+
 	FILE * f = fopen(argv[1], "rb");
+	
 	if (f == NULL) {
 		printf("Could not open file.\n");
 		printf("Was a file specified?\n");
@@ -14,21 +15,27 @@ int main(int argc, char ** argv) {
 		printf("Usage: brainfuck_monster myfile.b");
 		return 0;
 	}
+	
 	fseek(f, 0, SEEK_END); // end of file
 	long fsize = ftell(f); // file size
 	fseek(f, 0, SEEK_SET); // reset to beginning of file
+	
 	char * fstring = malloc(fsize + 1); // allocate
+	
 	if (fstring == NULL) {
 		printf("Allocation failed");
 		return 0;
 	}
+
 	fread(fstring, fsize, 1, f);     // get file data as string
 	fclose(f); 
 	fstring[fsize] = 0;              // null terminate
 	
 	// get if matching brackets
+	
 	int startbrack = 0;
 	int nbrackets = 0;
+	
 	for (insp = 0; insp < fsize; insp++) {
 		if (fstring[insp] == ']') {
 			if (startbrack >= 0) {
@@ -48,12 +55,15 @@ int main(int argc, char ** argv) {
 
 	int * go2 = malloc(fsize * sizeof(int));
 	int * skip2 = malloc(fsize * sizeof(int));
+	
 	if (go2 == NULL || skip2 == NULL) {
 		printf("allocation failed");
 		return 0;
 	}
+
 	int x;
 	int y;
+	
 	for (x = 0; x < fsize; x++) {
 		if (fstring[x] == '[') {
 			startbrack = 1;
@@ -71,6 +81,7 @@ int main(int argc, char ** argv) {
 			}
 		}
 	}
+	
 	startbrack = 0;
 	for (x = fsize-1; x >= 0; x--) {
 		if (fstring[x] == ']') {
@@ -93,14 +104,6 @@ int main(int argc, char ** argv) {
 	insp = 0;
 	int charsin = 0; // for debuggin
 	while (insp < fsize) {
-		// debuging
-		// printf("\nCell:%d Val: %d\n", cell, tape[cell]);
-		// printf("%s\n", fstring);
-		// for (charsin = 0; charsin < insp; charsin++) {
-		// 	putchar(' ');
-		// }
-		// putchar('|');
-		// getchar();
 		if (fstring[insp] == '+') {
 			tape[cell]++;
 		}
@@ -143,6 +146,7 @@ int main(int argc, char ** argv) {
 		}
 		insp++;
 	}
+	
 	free(fstring);
 	free(go2);
 	free(skip2);
